17f5ef31d7e52156615a0114058b1858c03fad90
Natan-Mesele <huluale12@gmail.com>
2023-11-09 15:16:13 +0300

    imageslider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from "styled-components"
+import ImageSlider from './ImageSlider'
 
 function Home() {
   return (
     <Container>
-      
+      <ImageSlider/>
     </Container>
   )
 }
@@ -12,5 +13,18 @@ function Home() {
 export default Home
 
 const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
 
+    &:before {
+        background: url("img/home-background.png") center center / cover no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+
+    }
 `
